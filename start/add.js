8ed090381ce1bd326b672ca2c9b8423bb65a7a8c
Tushar Jain <tusharjainqwerty@gmail.com>
2024-08-05 01:11:02 +0530

    initial commit

---
+++ start/add.js
@@ -0,0 +1,10 @@
+function enter() {
+
+// alert("Name the product you want to enter");
+// let a = prompt("Enter Name");
+// a=document.getElementsByClassName("enter1").value;
+
+
+
+
+}
---
+++ start/script.js
@@ -0,0 +1,16 @@
+function loginform()
+{
+   let user = document.getElementById("text1").value
+   let pass = document.getElementById("text2").value
+if(   user == "admin"
+       && pass == "12345" )
+       {
+        
+        location.href="index.html";
+       }
+       else
+       {
+        location.href="login.html";
+        alert("Wrong Credentials!")
+       }
+    }
