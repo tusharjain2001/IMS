93331842ccc362ec05a4dd8eeaebaa5241eb00be
Tushar Jain <tusharjainqwerty@gmail.com>
2024-08-05 01:16:53 +0530

    format doc

--- start/script.js
+++ start/script.js
@@ -1,16 +1,10 @@
-function loginform()
-{
-   let user = document.getElementById("text1").value
-   let pass = document.getElementById("text2").value
-if(   user == "admin"
-       && pass == "12345" )
-       {
-        
-        location.href="index.html";
-       }
-       else
-       {
-        location.href="login.html";
-        alert("Wrong Credentials!")
-       }
-    }
+function loginform() {
+  let user = document.getElementById("text1").value;
+  let pass = document.getElementById("text2").value;
+  if (user == "admin" && pass == "12345") {
+    location.href = "index.html";
+  } else {
+    location.href = "login.html";
+    alert("Wrong Credentials!");
+  }
+}
