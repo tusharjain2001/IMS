9774bd0a421cb1098cbd8c32689405b626598055
Tushar Jain <tusharjainqwerty@gmail.com>
2024-08-12 02:14:26 +0530

    updated bg

--- start/dialog.js
+++ start/dialog.js
@@ -76,8 +76,7 @@ const searchfun = () => {
 
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-      }
-      else{
+      } else {
         tr[i].style.display = "none";
       }
     }
